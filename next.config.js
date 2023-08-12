14550656052c056c8273cef529b50da5b53aacef
uzanenis <uzanenis@outlook.com>
2023-08-12 12:21:52 +0300

    feat: upload pdf and pdt2text

--- next.config.js
+++ next.config.js
@@ -1,5 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    webpack: (config) => {
+        config.module.rules.push({
+            test: /\.node/,
+            use: 'raw-loader',
+        });
+
+        return config;
+    },
     experimental: {
         serverActions: true,
     },
